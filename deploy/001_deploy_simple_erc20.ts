0792875f532a364ed2b961a794f928a33c2fb3fc
HUBS <51133323+mrheyday@users.noreply.github.com>
2022-05-07 09:55:23 +0200

    Initialize examples/fork-deploy

---
+++ deploy/001_deploy_simple_erc20.ts
@@ -0,0 +1,22 @@
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+import {DeployFunction} from 'hardhat-deploy/types';
+import {parseEther} from 'ethers/lib/utils';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const {deployments, getNamedAccounts} = hre;
+  const {deploy} = deployments;
+
+  console.log({live: hre.network.live});
+
+  const {deployer, simpleERC20Beneficiary} = await getNamedAccounts();
+
+  console.log({simpleERC20Beneficiary, deployer});
+
+  await deploy('SimpleERC20', {
+    from: deployer,
+    args: [simpleERC20Beneficiary, parseEther('1000000000')],
+    log: true,
+  });
+};
+export default func;
+func.tags = ['SimpleERC20'];
